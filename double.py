2a088f5710080f7c3456adb4341fc7df9780eb75
Josephtheelder <Josephtheelder@gmail.com>
2022-03-10 21:37:35 -0800

    update double.py

--- double.py
+++ double.py
@@ -1,9 +1,7 @@
-#doubler definition
 def doubler(n):
     return 2*n
 
-#Taking a number as an input
 n = int(input("Enter a number that you want to double : "))
-#Function call
 a = doubler(n)
+
 print(a) #prints the value after Function call
