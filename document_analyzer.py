c6b34844221f8e09836ca0d25bf308760f8a11b2
Josephtheelder <Josephtheelder@gmail.com>
2022-03-22 23:59:05 -0700

    new

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,9 +1,9 @@
-test_str="A few months ago, I decided to deaccession an assortment of my things by whatever means feasible: selling, donating, recycling, giving them away, losing them on the subway, or reserving a spot for them on the next Mars Explorer.  gathered my unwanteds and piled them in the living room. A fraction of what was in that jumble: seven antique glass cake stands that belonged a  mother; a dormitory’s worth new sheet sets and blankets for a bed size that is not mine; a set of Lenox china that my grandmother gave to my mother, who gave it to me, and was never used; clothes galore; a Viking stove grate that arrived cracked, and which I saved because I planned to weld it into a sculpture someday, after I learned how to weld; several rolls of Trump toilet paper I wrongly thought were amusing a few years ago. I wish I could have added my boyfriend’s too large Le Corbusier lounger. (There are Web sites, such as NeverLikedItAnyway.com, that will buy your ex’s leavings, ranging from engagement rings to “Rick and Morty” socks.\n)"
+test_str="A few months ago, I decided to deaccession an assortment of my things by whatever means feasible: selling, donating, recycling, giving them away, losing them on the subway, or reserving a spot for them on the next Mars Explorer.  gathered my unwanteds and piled them in the living room. A fraction of what was in that jumble: seven antique glass cake stands that belonged a  mother; a dormitory’s worth new sheet sets and blankets for a bed size that is not mine; a set of Lenox china that my grandmother gave to my mother, who gave it to me, and was never used; clothes galore; a Viking stove grate that arrived cracked, and which I saved because I planned to weld it into a sculpture someday, after I learned how to weld; several rolls of Trump toilet paper I wrongly thought were amusing a few years ago. I wish I could have added my boyfriend’s too large Le Corbusier lounger. (There are Web sites, such as NeverLikedItAnyway.com, that will buy your ex’s leavings, ranging from engagement rings to “Rick and Morty” socks.\r)"
 punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
  
 for ele in test_str:
     if ele in punc:
-        test_str = test_str.replace(ele, "")
+        test_str = test_str.replace(ele, "\r")
  
 
 l=test_str.split()
@@ -21,5 +21,5 @@ j=0
 for i in sortdict.keys():
   if j==5:
     break
-  print(str(i)+": "+str(sortdict[i]))     
+  print(str(i)+" : "+str(sortdict[i]))     
   j+=1
